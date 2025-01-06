f2ea81978b697a68971cbd2f09ca0e41fe8228fe
Akshayy <akshayrushi2@gmail.com>
2025-01-06 10:31:17 +0530

    Day-4
    smth

---
+++ Day-4/DSA/src/com/mru/loops/Loops.java
@@ -0,0 +1,23 @@
+package com.mru.loops;
+
+public class Loops {
+	public static void main(String args[]) {
+		int arr1[] = {100,200,300,400,500};
+		int el = arr1[0];
+		System.out.println("0th Index Element: "+el);
+		 el = arr1[1];
+		System.out.println("1th Index Element: "+el);
+		 el = arr1[2];
+		System.out.println("2th Index Element: "+el);
+		 el = arr1[3];
+		System.out.println("3th Index Element: "+el);
+		 el = arr1[4];
+		System.out.println("4th Index Element: "+el);
+		System.out.println("The length of the array: "+arr1.length);
+		for(int i=0;i<arr1.length;i++) {
+			System.out.print(arr1[i]+" ");
+		}
+		System.out.println();
+		
+	}
+}

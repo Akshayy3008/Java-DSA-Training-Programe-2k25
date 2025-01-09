e66c71a676a9dc99c613ef1e2c44bce5b393b419
Akshayy <akshayrushi2@gmail.com>
2025-01-09 11:17:59 +0530

    7 Lines of code

--- Day-7/Testt/src/com/mru/smth/smth.java
+++ Day-7/Testt/src/com/mru/smth/smth.java
@@ -2,6 +2,7 @@ package com.mru.smth;
 
 public class smth {
 	public static void main(String args[]) {
-		System.out.print("hi");
+		System.out.print("CSE batch");
+		System.out.print("CSE batch");
 	}
 }

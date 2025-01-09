aa39b0e11c8458ea52803a46746faf4fef159db9
Akshayy <akshayrushi2@gmail.com>
2025-01-09 11:42:04 +0530

    .

--- Day-7/Testt/src/com/mru/smth/smth.java
+++ Day-7/Testt/src/com/mru/smth/smth.java
@@ -3,6 +3,7 @@ package com.mru.smth;
 public class smth {
 	public static void main(String args[]) {
 		System.out.print("CSE batch");
+		System.out.print("CSE batch1");
 		System.out.print("CSE batch");
 	}
 }
